4feb4a1d9ad396486796c887bc79d249b5a10acf
Thiago Silva <thiago@Thiagos-MacBook-Air.local>
2024-05-07 22:23:56 -0300

    styling home

--- src/views/home/Home.js
+++ src/views/home/Home.js
@@ -1,12 +1,17 @@
 import { render } from "@testing-library/react";
 import React, { Component } from "react";
 import '../../css/App.css';
+import '../../css/Texts.css';
 
 class Home extends Component {
     render() {
         return(
-            <div className="box_container">
-
+            <div className="box_container box_shadow text_centered">
+                <div className="profile_pic"></div>
+                <h1 className="text_teal_color">Olá, eu sou Thiago</h1>
+                <span className="full_width">
+                    <p>Desenvolvedor <i className="text_light text_cutted">front-end</i> mobile</p>
+                </span>
             </div>
         );
     };
